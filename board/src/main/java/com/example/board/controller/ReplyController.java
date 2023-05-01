4ddac76184e6f06cb9fbbc578023aba85ff5a87f
BEOMS <jbs980612@gmail.com>
2023-05-02 01:00:51 +0900

    REST Controller 제작: 댓글목록,조회,삭제

--- board/src/main/java/com/example/board/controller/ReplyController.java
+++ board/src/main/java/com/example/board/controller/ReplyController.java
@@ -33,9 +33,30 @@ public class ReplyController {
 	public ResponseEntity<String> create(@RequestBody ReplyVO replyVO) { //json이랑 Reply가 같아야함 , produces = 결과값을 리턴할때 값을 적어주는것(브라우저에서는
 		//글을 String 말고 TEXT_PLAIN_VALUE로 받음 //ResponseEntity = 응답할때 여러가지 요소들을 사용하는것,서버의상태를 알려주는것
 		//나는 success를 주기위해서 <String>을 사용함, 숫자를 사용하려면 <int>
+		//Header에 담겨오면 @RequestBody
 		log.info("create...... : " + replyVO);
 		return replyService.register(replyVO) ? new ResponseEntity<>("success", HttpStatus.OK) : //성공했을때 (register가 boolean임)
 			new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR); //실패했을때
+	}
+// 게시글 댓글 전체 조회(bno번호로 조회)
+	@GetMapping(value="/{bno}",produces = {MediaType.APPLICATION_PROBLEM_XML_VALUE, MediaType.APPLICATION_JSON_UTF8_VALUE})
+	public ResponseEntity<List<ReplyVO>> getList(@PathVariable("bno") Long bno, @PathVariable int page) {	
+		//URL에 담겨오면 @PathVariable  //XML로 하거나 JSON으로 받거나 두개다 설정함 (데이터가옴)
+		log.info("getList.....: " + bno);
+		return new ResponseEntity<>(replyService.findAllByBNO(bno), HttpStatus.OK);
+	}
+// 댓글 1개 조회(rno번호로 조회)
+	@GetMapping(value="/{rno}", produces = {MediaType.APPLICATION_JSON_UTF8_VALUE})
+	public ReplyVO getReply(@PathVariable("rno") Long rno) {
+		log.info("getReply........ : " + rno);
+		return replyService.findByRNO(rno);
+		//ResponseEntity 대신 바로 ReplyVO를 사용함
+	}
+// 댓글 삭제
+	@DeleteMapping(value = "/{rno}", produces = MediaType.TEXT_PLAIN_VALUE) //댓글삭제할때는 GET,POST가능, 근데 메소드방식을 DELETE로 사용해도됌(가독성)
+	public String remove(@PathVariable Long rno) {
+		log.info("remove.......: " + rno);
+		return replyService.remove(rno) ? "success" : "fail";
 	}
 	
 }
